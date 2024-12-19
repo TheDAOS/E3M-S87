34100e1a1bca2bb1feccda261f42ada7e9144145
theDAOS <sanjusanthosh46@gmail.com>
2024-12-19 09:41:26 +0530

    historical sales data

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ class Products {
     constructor() {
         this.productDetails = {};
         this.productID = 1000;
+        this.salesData = [];
     }
 
     addProduct(name, price, inventoryLevel, demand) {
@@ -16,20 +17,40 @@ class Products {
                 price,
                 inventoryLevel,
                 demand,
-            }
+            };
             console.log(`${name} inventory added.`);
         } else {
             console.log(`Enter Valid Data.`);
         }
     }
 
-    
+    sellingProduct(id, sold) {
+        if (this.productDetails[id] && this.productDetails[id].inventoryLevel >= sold) {
+            this.productDetails[id].inventoryLevel -= sold ;
+            this.salesData.push({
+                name: this.productDetails[id].name,
+                price: this.productDetails[id].price,
+                quantity: sold,
+                totalPrice: this.productDetails[id].price * sold,
+                demand: this.productDetails[id].demand,
+            });
+            console.log(`Sold ${sold} ${this.productDetails[id].name}`);
+            
+        } else {
+            console.log(`Enter Valid Data.`);
+        }
+    }
+
+    // calculateDemand() {
+    //     setInterval(() => {
+    //         if ()
+    //     }, 30000);
+    // }
 
 }
 
 // 2. Demand Tracking
 
-//     Use arrays to store historical sales data for each product.
 //     Track product sales over time to calculate real-time demand.
 
 
@@ -38,4 +59,7 @@ const ec = new Products();
 ec.addProduct("Noodle", 70, 10, "Normal");
 ec.addProduct("Soap", 40, 20, "Low");
 
+console.log(ec);
+
+ec.sellingProduct(1001, 2);
 console.log(ec);
